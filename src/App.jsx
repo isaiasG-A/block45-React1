6b2ddc707e05c1aa8ff1f7660042ee0ff050de6b
isaiasG-A <isaiasgarciaariza@gmail.com>
2023-06-15 17:50:59 -0700

    puppy names are now being rendered on the screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css';
+import { useState } from 'react'
+import {puppyList} from './data.js';
 
 function App() {
-
+  const [puppies, setPuppies] = useState(puppyList);
+    //puppies: value being stored.
+    // setPuppies: function to reset values.
+  console.log(puppies)
   return (
     <>
       <div>
-       
+       {puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>
+       })}
       </div> 
     </>
   )
