eb17de28415a030e2ac3103931f7f9e6f66caad0
isaiasG-A <isaiasgarciaariza@gmail.com>
2023-06-15 20:50:41 -0700

    select a feature puppy section has been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
 import './App.css';
 import { useState } from 'react'
-import {puppyList} from './data.js';
+import { puppyList } from './data.js';
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-    //puppies: value being stored.
-    // setPuppies: function to reset values.
-  console.log(puppies)
+  //puppies: value being stored.
+  // setPuppies: function to reset values.
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
   return (
     <>
       <div>
        {puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>
+        return (
+        <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+        )
        })}
+       {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )} 
       </div> 
     </>
   )
+  
 }
 
 export default App
